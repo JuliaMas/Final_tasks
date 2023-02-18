19a03cb76a48add60f24178e7c67595c5c3c929e
Julia <julia.masaeva@gmail.com>
2023-02-18 09:57:46 +0600

    Показать натуральные числа от N до 1, N задано

---
+++ Task002/Program.cs
@@ -0,0 +1,17 @@
+﻿// Показать натуральные числа от N до 1, N задано
+Console.WriteLine("Введите N: ");
+int N = int.Parse(Console.ReadLine());
+int count = 2;
+PrintNumber(N, count);
+Console.Write(1);
+
+void PrintNumber(int N, int count)
+{
+  if (count > N) return;
+  else
+  {
+    PrintNumber(N, count + 1);
+    Console.Write(count + " ");
+  }
+  
+}
