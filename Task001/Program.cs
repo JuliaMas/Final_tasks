6db654be4b4971d4992349cf49bb726b7e348212
Julia <julia.masaeva@gmail.com>
2023-02-18 09:44:17 +0600

    Показать натуральные числа от 1 до N, N задано

---
+++ Task001/Program.cs
@@ -0,0 +1,16 @@
+﻿// Показать натуральные числа от 1 до N, N задано
+Console.WriteLine("Введите N: ");
+int N = int.Parse(Console.ReadLine());
+PrintNum(N);
+
+void PrintNum(int N) 
+{
+  if (N <= 0) 
+  return;
+  else
+  {
+    PrintNum(N - 1);
+    Console.Write(N + " ");
+  }
+  
+}
