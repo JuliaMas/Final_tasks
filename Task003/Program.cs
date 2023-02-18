5e8dad8cb727871c8eba2ef7967374a468892872
Julia <julia.masaeva@gmail.com>
2023-02-18 11:38:09 +0600

    Показать натуральные числа от M до N, N и M заданы

--- Task003/Program.cs
+++ Task003/Program.cs
@@ -22,35 +22,3 @@ void PrintNum(int m, int n)
 }
 
 
-/*while (true)
-{
-int m = InputNumbers("Введите m: ");
-int n1 = InputNumbers("Введите n: ");
-int temp = 0;
-if (m > n1) 
-{
-    temp = m;
-    m = n1;
-    n1 = temp; 
-  // n = m;
-  // m = n1;
-  // n1 = n;
-}
-else{ 
-PrintNumber1(m, n1);
-break;
-}
-}
-
-void PrintNumber1(int m, int n1) 
-{
-  if (n1 < m) return;
-  PrintNumber1(m, n1 - 1);
-  Console.Write(n1 + " ");
-}
-int InputNumbers(string input) 
-{
-  Console.Write(input);
-  int output = Convert.ToInt32(Console.ReadLine());
-  return output;
-}*/
