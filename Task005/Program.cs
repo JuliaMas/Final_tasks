c8592bc9f020c54a68d68d75182097a88e5f1d59
Julia <julia.masaeva@gmail.com>
2023-02-18 10:48:39 +0600

    Найти сумму цифр числа

---
+++ Task005/Program.cs
@@ -0,0 +1,13 @@
+﻿// Найти сумму цифр числа
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int sum = Summ(number);
+int Summ(int number)
+{
+    if(number > 0) 
+    return 
+    number % 10 + Summ(number / 10);
+    else 
+    return 0;
+}
+Console.WriteLine($" {sum}");
